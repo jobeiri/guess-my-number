fab8498cac5fa076876ad9172372f1643aa317d5
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2020-12-15 01:03:02 -0800

    8. Manipulating CSS Styles

--- script.js
+++ script.js
@@ -24,10 +24,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.transition = 'all 0.2s ease-in-out';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -37,6 +46,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🧨 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is too high
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low';
