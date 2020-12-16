f23d868d1da04e75a535fd46d23c756523ebf504
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2020-12-16 09:37:59 -0800

    11. Refactoring Our Code The DRY Principle

--- script.js
+++ script.js
@@ -20,13 +20,17 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
@@ -41,21 +45,12 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-
-    // When guess is too high
-  } else if (guess > secretNumber) {
+  }
+  // When guess is different
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🧨 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // When guess is too high
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
